bf4083bf3eabbada64a8bb213c91b659fb64bd24
Elias Akalu <143254520+sdhbdhf@users.noreply.github.com>
2024-11-28 21:37:49 +0300

    First Project Uploading
    This is the first day I started uploading my projects formally that I had done before.

---
+++ Homework.js
@@ -0,0 +1,40 @@
+
+function updateHeading(newHeading)
+{
+    let h1=document.querySelector("h1");
+    h1.innerHTML=newHeading;
+}
+function changer()
+{
+    let place=prompt("Which city do you live in?");
+    let temprature=prompt(`What is the current temprature in °c  ${place}`);
+    if (temprature<=0)
+{
+    updateHeading("");
+        
+    updateHeading(`🥶<br>Currently ${temprature}° in ${place}`);
+
+    
+}
+else if(temprature>30)
+{
+    updateHeading("");
+    updateHeading(`🥵<br>Currently ${temprature}° in ${place}`);
+}
+else if(temprature>0 && temprature<=30)
+{
+    // updateHeading("");
+    updateHeading(`😎<br>Currently ${temprature}° in ${place}`);
+}
+else{
+    alert("Please enter only numercal data:");
+    changer();
+}
+
+}
+let buttonClick=document.querySelector("button");
+butttonClick:addEventListener("click",changer);
+
+
+
+
