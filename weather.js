b436886f9529ba80f736de2386e9e47c87ccc5ba
Elias Akalu <143254520+sdhbdhf@users.noreply.github.com>
2025-02-03 22:04:03 +0300

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,39 +1,53 @@
 
-function updateHeading(newHeading)
-{
-    let h1=document.querySelector("h1");
-    h1.innerHTML=newHeading;
-}
-function changer()
-{
-    let place=prompt("Which city do you live in?");
-    let temprature=prompt(`What is the current temprature in °c  ${place}`);
-    if (temprature<=0)
-{
-    updateHeading("");
-        
-    updateHeading(`🥶<br>Currently ${temprature}° in ${place}`);
-
-    
-}
-else if(temprature>30)
-{
-    updateHeading("");
-    updateHeading(`🥵<br>Currently ${temprature}° in ${place}`);
-}
-else if(temprature>0 && temprature<=30)
-{
-    // updateHeading("");
-    updateHeading(`😎<br>Currently ${temprature}° in ${place}`);
-}
-else{
-    alert("Please enter only numercal data:");
-    changer();
+function updateHeading(newHeading) {
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = newHeading;
 }
 
+function getWeather(lat, lon) {
+    const apiKey = "YOUR_API_KEY";
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const temperature = data.main.temp;
+            const place = data.name; 
+
+            // Determine the emoji based on the temperature
+            if (temperature <= 0) {
+                updateHeading(`🥶<br>Currently ${temperature}° in ${place}`);
+            } else if (temperature > 30) {
+                updateHeading(`🥵<br>Currently ${temperature}° in ${place}`);
+            } else if (temperature > 0 && temperature <= 30) {
+                updateHeading(`😎<br>Currently ${temperature}° in ${place}`);
+            }
+        })
+        .catch(error => {
+            alert("Error fetching weather data.");
+            console.error(error);
+        });
 }
-let buttonClick=document.querySelector("button");
-butttonClick:addEventListener("click",changer);
+function changer() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+
+            getWeather(lat, lon);
+        }, () => {
+            alert("Geolocation is not supported or permission denied.");
+        });
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+let buttonClick = document.querySelector("button");
+buttonClick.addEventListener("click", changer);
+
+
+
 
 
 
